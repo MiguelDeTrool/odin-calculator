95733e9ff4b799258712f27a4028f29679cfee2b
MiguelDeTrool <migueldetrool@gmail.com>
2022-02-08 01:00:33 +0100

    Add operator function declaration and separate script into multiple files

---
+++ src/operatorDeclarations.js
@@ -0,0 +1,27 @@
+function sum(a, b) {
+    a = parseFloat(a);
+    b = parseFloat(b);
+    let result = a + b;
+    return result;
+}
+
+function subtract(a, b) {
+    a = parseFloat(a);
+    b = parseFloat(b);
+    let result = a - b;
+    return result;
+}
+
+function multiply(a, b) {
+    a = parseFloat(a);
+    b = parseFloat(b);
+    let result = a * b;
+    return result;
+}
+
+function divide(a, b) {
+    a = parseFloat(a);
+    b = parseFloat(b);
+    let result = a / b;
+    return result;
+}
